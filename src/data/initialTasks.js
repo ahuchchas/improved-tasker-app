f20e8f558b76bfefc4c6a3e5aa1da8a914b78642
Abu Hurayra Uchchas <ahuchchas@outlook.com>
2024-01-27 23:14:39 +0600

    more dummy task added

--- src/data/initialTasks.js
+++ src/data/initialTasks.js
@@ -16,6 +16,23 @@ export const initalTasks = [
     priority: "High",
     isFavourite: true,
   },
+  {
+    id: crypto.randomUUID(),
+    title: "Buying pens",
+    description:
+      "Buy some color pen and some regular black pens when I go to market",
+    tags: ["Color pen", "Black pen"],
+    priority: "Low",
+    isFavourite: false,
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "Painting the house",
+    description: "Get a painter within this month to paint the house",
+    tags: ["Paint", "House", "This month"],
+    priority: "Medium",
+    isFavourite: false,
+  },
 ];
 
 /** object structure
